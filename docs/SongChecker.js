1f29c1dbe6729b48083159f0864575c433bc8c4f
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2023-08-15 17:21:07 -0600

    Update SongChecker.js

--- docs/SongChecker.js
+++ docs/SongChecker.js
@@ -9,6 +9,11 @@ document.getElementById("btn").addEventListener("click", () => {
           "secret": "Wmfd2893gb7",
           "songID": str
         };
+        // Send Request
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(data));
         
       }else {
         alert('Enter Valid Song Id');
