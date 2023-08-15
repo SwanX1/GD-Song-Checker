4cf8a55b9fc773b612b80e2f305a03ed2b387b37
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2023-08-15 12:41:21 -0600

    me when short

--- docs/SongChecker.js
+++ docs/SongChecker.js
@@ -1,21 +1,15 @@
-var submit = document.getElementById("btn");
-var SongId = document.getElementById("Sid");
 var returndata = document.getElementById("SongValue");
-// Get Song Data
-function getSong(id) {
-  var url = `http://www.boomlings.com/database/getGJSongInfo.php`;
-  data = {
-    "secret": "Wmfd2893gb7",
-    "songID": id
-  }
-}
-// Click Detector
-submit.addEventListener("click", () => {
-  var str = SongId.value;
+document.getElementById("btn").addEventListener("click", () => {
+  var str = document.getElementById("Sid").value
   if (!str) {alert('Enter Valid Song Id');}
     else {
       if (!isNaN(str) && !isNaN(parseFloat(str))) {
-        get(str)
+        var url = `http://www.boomlings.com/database/getGJSongInfo.php`;
+        data = {
+          "secret": "Wmfd2893gb7",
+          "songID": str
+        };
+        
       }else {
         alert('Enter Valid Song Id');
       }
